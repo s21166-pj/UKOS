51ac09f62c492b604ca04931dae1bcece26f5e85
badkowski <s21166@pjwstk.edu.pl>
2019-12-14 13:32:23 +0100

    Post Code gen huehuehue

--- main.py
+++ main.py
@@ -2,3 +2,22 @@
 
 print("Hello World")
 
+def post_code_gen(x: str, y: str):
+
+    x = x.split("-")
+    x = ''.join(x)
+    x = int(x)
+
+    y = y.split("-")
+    y = ''.join(y)
+    y = int(y)
+
+
+    generated_post_codes = []
+
+    for post_code in range(x, y + 1):
+        generated_post_codes.append(post_code)
+        print ('%02d-%03d' % (post_code/1000, post_code%1000))
+
+post_code_gen("79-900", "80-155")
+
